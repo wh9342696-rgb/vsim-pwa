228ff6db8b721d818481600350f90dae03627133
wh9342696-rgb <wh9342696@gmail.com>
2026-09-23 01:47:57 +0300

    Expose service worker registration to PWABuilder

--- sw.js
+++ sw.js
@@ -35,6 +35,21 @@ self.addEventListener('fetch', event => {
   const requestUrl = new URL(event.request.url);
   if (event.request.method !== 'GET' || requestUrl.pathname.startsWith('/api/')) return;
 
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          if (response.ok) {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy));
+          }
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
   event.respondWith(
     fetch(event.request)
       .then(response => {
