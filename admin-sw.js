89c198cc38166cabf15eb054c0ce76b3aecf91b0
wh9342696-rgb <wh9342696@gmail.com>
2026-09-23 12:44:33 +0300

    Refresh admin PWA cache for mobile deposits

--- admin-sw.js
+++ admin-sw.js
@@ -1,5 +1,5 @@
 // VSIM Admin Panel Service Worker
-const CACHE_NAME = 'vsim-admin-v3.2-package-fix';
+const CACHE_NAME = 'vsim-admin-v3.3-mobile-deposits';
 const ASSETS_TO_CACHE = [
   '/admin/',
   '/admin/index.html',
